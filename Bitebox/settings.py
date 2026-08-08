59d3eed95a27985e990521e489ab5247931fe9a8
eshwarisaradgi09-boop <eshwarisaradgi09@gmail.com>
2026-08-08 13:45:27 +0530

    Fix static files for Railway

--- Bitebox/settings.py
+++ Bitebox/settings.py
@@ -120,11 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-import os
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "delivery" / "static",
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
